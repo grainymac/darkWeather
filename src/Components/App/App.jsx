6d2ca3179f42dd209fdfb70a3182aa3421b83ec3
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-06-12 06:07:43 -0500

    added api calls and very little styling

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react'
+import CurrentWeather from '../CurrentWeather/CurrentWeather'
+import Forecast from '../Forecast/Forecast'
+import Search from '../Search/Search'
+import { WEATHER_API_URL, WEATHER_API_KEY } from '../../api'
 import './App.css'
 
 const App = () => {
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
 
-  const apiKey = '23448f48b958ddb737240a98bf3344fe'
-  const [weatherData, setWeatherData] = useState([{}])
-  const [city, setCity] = useState('')
+  const handleOnSearchChange = (searchData) => {
+    const [latitude, longitude] = searchData.value.split(' ')
 
-  const getWeather = (event) => {
-    if (event.key === 'Enter') {
-      fetch(`https://api.openweathermap.org/data/3.0/weather?q=${city}&units=imperial&APPID=${apiKey}`)
-      .then(response => response.json())
-      .then(data => {
-        setWeatherData(data)
-      }
-      )
-    }
+    const getCurrentWeather = fetch(`${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
+
+    const getForecast = fetch(`${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([getCurrentWeather, getForecast])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json()
+        const forecastResponse = await response[1].json()
+
+        setCurrentWeather({city: searchData.label, ...weatherResponse})
+        setForecast({city: searchData.label, ...forecastResponse})
+      })
+      .catch((err) => console.error(err))
   }
+
+  console.log(currentWeather)
+  console.log(forecast)
 
   return (
     <div className='container'>
-      <input className='input' placeholder='Enter City...' onChange={e => setCity(e.target.value)} value={city} onKeyPress={getWeather}/>
+      <Search onSearchChange={handleOnSearchChange}/>
+      <CurrentWeather />
     </div>
   )
 }
---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd7635eff76msha229d0bb071e0bdp13e939jsn65edae4c49b7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+
+export const WEATHER_API_KEY = '23448f48b958ddb737240a98bf3344fe'
