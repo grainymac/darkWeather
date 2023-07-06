134a5dbda77bc3519ccc4fcc757adc9edd6720bf
Ian McIntosh <imcintosh22@turing.edu>
2023-07-06 20:41:32 +0000

    started forecast component

--- src/Components/Forecast/Forecast.jsx
+++ src/Components/Forecast/Forecast.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { faArrowDown } from 'react-icons/fa'
+import '/Forecast.css'
 
 const Forecast = () => {
   return (
     <div className='section section__forecast'>
       <div className='forecast__card'>
-        <div className='forecast__card-icon'></div>
+        <div className='forecast__card-icon'>
+          <FaArrowDown />
+          <small>min</small>
+        </div>
+        <h2>0°C</h2>
       </div>
     </div>
   )
