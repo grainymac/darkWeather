6f15e578024efa4be847d461dff2f1a760e5b622
Ian McIntosh <imcintosh22@turing.edu>
2023-07-11 16:12:12 +0000

    adding loaders

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -82,7 +82,7 @@ const App = () => {
           )}
         </div>
       </div>
-      <Loader type='ball-scale-ripple-multiple' />
+      <Loader type='ball-clip-rotate-multiple' />
     </>
   )
 }
