5868f6c3e77e0022a2d7d0d50b122c905396d094
Ian McIntosh <imcintosh22@turing.edu>
2023-07-11 14:36:16 +0000

    responsive design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     return (
         <div className='footer'>
             <h3 className='footer-message'>developed by</h3><h2 className='darkdad'>DarkDad</h2>
+            <p>© 2023</p>
         </div>
     )
 }
